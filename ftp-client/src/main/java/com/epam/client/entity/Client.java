3b6481167fbaa6641ea8ea3cadb22e706cbfc029
Kirill_Kaluga <Kirill_Kaluga@epam.com>
2016-06-16 17:13:32 +0300

    added command design pattern

--- ftp-client/src/main/java/com/epam/client/entity/Client.java
+++ ftp-client/src/main/java/com/epam/client/entity/Client.java
@@ -1,10 +1,15 @@
 package com.epam.client.entity;
 
+import com.epam.client.entity.commands.Command;
+
+import java.util.HashMap;
+
 /**
  * Created by Kirill_Kaluga on 6/16/2016.
  */
 public class Client {
     private static Client uniqueInstance;
+    private HashMap<String, Command> commands;
 
     private Client() {}
 
